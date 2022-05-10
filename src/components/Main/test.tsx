c4bebfdc3cd103322a436f888b1afbd2a23a853b
Johnny <61324058+Johnny-Brendow@users.noreply.github.com>
2022-05-10 11:51:52 -0300

    criando todo projeto , porem nem tudo esta configurado

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import { Main } from './index'
+
+describe('<Main/>', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
+    expect(screen.getByRole('heading', { name: /asdf/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document'
+
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const page = ctx.renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    )
+
+    const styleTags = sheet.getStyleElement()
+
+    return { ...page, styleTags }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
